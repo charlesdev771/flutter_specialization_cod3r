e0a330e7f0765fe990b8d9bd9fb473a5b1cc63ec
Carlos Charles Oliveira Dantas <charles.d@academico.ifrn.edu.br>
2023-01-31 20:57:56 -0300

    Final day 2

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -15,7 +16,14 @@ class ExpensesApp extends StatelessWidget {
 
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  MyHomePage({super.key});
+
+
+
+  final _transactions = [
+    Transaction(id: '1', title: "PC", value: 2200, date: DateTime.now(), ), 
+    Transaction(id: '2', title: "Monitor", value: 150, date: DateTime.now(), ), 
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -23,8 +31,62 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Despesas Pessoais"),
       ),
-      body: Center(
-        child: Text("0.001"),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceAround, 
+        children: [
+          Container(
+            child: Card(
+              color: Colors.blue, 
+              child: Text("Grafico"),
+              elevation: 5,
+            ), 
+          ),
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15, 
+                        vertical: 10, 
+                      ), 
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple, 
+                          width: 2, 
+                        )
+                      ), 
+                      
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        tr.value.toStringAsFixed(2),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold, 
+                          fontSize: 20, 
+                          color: Colors.purple, 
+                        ),
+                      ),
+                    ), 
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(tr.title, style: TextStyle(
+                          fontSize: 16, 
+                          fontWeight: FontWeight.bold,
+                        )), 
+                        Text(tr.date.toString(), style: TextStyle(
+                          color: Colors.grey,
+                        ),),
+                      ], 
+                    )
+                  ],
+                )
+              );
+            }).toList(),
+          )
+        ],
       )
     );
   }
