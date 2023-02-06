4c12c3e03ad9ff6ca94e406653a30701e2f6c00a
Carlos Charles Oliveira Dantas <charles.d@academico.ifrn.edu.br>
2023-02-06 12:31:20 -0300

    route

---
+++ shop/lib/components/category_item.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import '../models/category.dart';
+import '../screens/categoies_meals_screen.dart';
+
+class CategoryItem extends StatelessWidget {
+
+  void _selectCategory(BuildContext context) 
+  {
+    Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+      return CategoriesMealsScreen(category);
+    } ));
+  }
+
+  final Category category;
+
+  CategoryItem(this.category);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      borderRadius: BorderRadius.circular(15),
+      splashColor: Theme.of(context).primaryColor,
+      child: Container(
+          padding: EdgeInsets.all(15),
+          child: Text(category.title),
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.7),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ))),
+    );
+  }
+}
